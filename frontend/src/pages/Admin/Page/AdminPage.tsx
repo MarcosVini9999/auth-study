064f05c6ed6267f32aad09f6b80c3cc8705ac2e3
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2023-07-20 13:50:11 -0300

    feat: admin delete users

--- frontend/src/pages/Admin/Page/AdminPage.tsx
+++ frontend/src/pages/Admin/Page/AdminPage.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { deleteUser, getUsers } from "../../../utils/user";
+import { iUser } from "../../../config/types";
 
 export const AdminPage: React.FC = () => {
+  const [users, setUsers] = React.useState<iUser[]>([]);
+
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await getUsers().then((users) => users.filter((user: iUser) => !user.admin));
+      setUsers(users);
+    };
+
+    if (!users.length) fetchUsers();
+  }, []);
+
   return (
     <React.Fragment>
-      <h1>Page adm</h1>
+      {users.length
+        ? users.map((user) => (
+            <div key={user.id}>
+              <p>{user.id}</p>
+              <p>{user.name}</p>
+              <p>{user.email}</p>
+              <button
+                onClick={() => {
+                  deleteUser(user.id);
+                  setUsers(users.filter((u) => u.id !== user.id));
+                }}
+              >
+                delete
+              </button>
+            </div>
+          ))
+        : null}
     </React.Fragment>
   );
 };
