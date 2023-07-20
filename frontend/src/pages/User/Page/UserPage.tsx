6c602958ac027b2b1e186830905869b212d2e310
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2023-07-20 09:49:00 -0300

    feat: user update

--- frontend/src/pages/User/Page/UserPage.tsx
+++ frontend/src/pages/User/Page/UserPage.tsx
@@ -1,15 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDebounce } from "../../../hooks/useDebounce";
+import { login, updateUser } from "../../../utils/user";
 
 export const UserPage: React.FC = () => {
   const userData = JSON.parse(localStorage.getItem("chaveDeUsuário") || "{}");
-  const [username, setUsername] = useState(userData.name);
-  const [email, setEmail] = useState(userData.email);
-  const [password, setPassword] = useState(userData.password);
+  const [username, setUsername] = React.useState(userData.name);
+  const [email, setEmail] = React.useState(userData.email);
+  const [password, setPassword] = React.useState(userData.password);
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setUsername(event.target.value);
+  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value);
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setPassword(event.target.value);
+
+  const debouncedUsername = useDebounce(username, 500);
+  const debouncedEmail = useDebounce(email, 500);
+  const debouncedPassword = useDebounce(password, 500);
+
+  const putUser = async () => {
+    const response = await updateUser(userData.id, username, email, password);
+    if (response) {
+      const user = await login(email, password, false);
+      localStorage.setItem("chaveDeUsuário", JSON.stringify(user));
+    } else alert("Sinto muito, ocorreu um erro ao atualizar!");
+  };
+
+  React.useEffect(() => {
+    putUser();
+  }, [debouncedUsername, debouncedEmail, debouncedPassword]);
 
   return (
     <React.Fragment>
-      <h1>User Page</h1>
+      <p>Username:</p>
+      <input type="text" value={username} onChange={handleUsername} />
+      <p>Email:</p>
+      <input type="email" value={email} onChange={handleEmail} />
+      <p>Password:</p>
+      <input type="password" value={password} onChange={handlePassword} />
     </React.Fragment>
   );
 };
